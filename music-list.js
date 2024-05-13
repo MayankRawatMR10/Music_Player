47228084c5cb191bf86ba4fca22ba7b0886f5b86
MayankRawat <118156379+MayankRawatMR10@users.noreply.github.com>
2024-05-13 13:04:51 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+    {
+        name: "Tune Jo Na Kaha",
+        artist: "Pritam, Mohit Chauhan",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "The Night We Met",
+        artist: "Lord Huron",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Tum Ho",
+        artist: "Mohit Chauhan",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "The Lazy Song",
+        artist: "Bruno Mars",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Phir Mohobbat",
+        artist: "Arijit Singh",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Money Trees",
+        artist: "Kendrick Lamar",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Chahu Mai Ya Na",
+        artist: "Palak, Arijit Singh",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Let Her Go",
+        artist: "The Passengers",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Jee Le Zara",
+        artist: "Vishal Dadlani",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Lovers Rock",
+        artist: "TV Girl",
+        img: "music-10",
+        src: "music-10"
+    },
+];
